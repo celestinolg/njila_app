f2c242ecca9af67a15397f613fa5779811004542
Celestino Langa <celestino.langa@jupiter.co.ao>
2025-08-21 17:27:53 +0100

    feature: home screen, top section info

--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -1,16 +1,309 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double indicatorValue = 0.8;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Tela Principal')),
-      body: Center(
-        child: Text(
-          'Bem-vindo à Tela Principal!',
-          style: TextStyle(fontSize: 24),
+      backgroundColor: Color(0xFF28333F),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF7B61FF).withAlpha(197),
+        leading: Container(
+          width: 48,
+          height: 48,
+          color: Colors.transparent,
+          child: Icon(Icons.menu, color: Colors.white),
+        ),
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 24,
+              backgroundImage: AssetImage('assets/img/user_img.png'),
+            ),
+            const SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Olá!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const Text(
+                  'Utilizador',
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                ),
+              ],
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.inbox_outlined, color: Colors.white, size: 24),
+            onPressed: () {
+              // Ação para notificações
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.mark_email_unread_outlined,
+              color: Colors.white,
+              size: 24,
+            ),
+            onPressed: () {
+              // Ação para configurações
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 416.0,
+                decoration: BoxDecoration(
+                  color: Color(0xFF7B61FF).withAlpha(200),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFF7B61FF),
+                      Color(0xFF7B61FF).withAlpha(50),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text.rich(
+                                      TextSpan(
+                                        text: '14,000/',
+                                        style: TextStyle(
+                                          color: Colors.white54,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        children: [
+                                          TextSpan(
+                                            text: '15,000',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text: ' steps',
+                                            style: TextStyle(
+                                              color: Colors.white54,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Text(
+                                      'Level 5',
+                                      style: TextStyle(
+                                        color: Colors.yellow.shade600,
+                                        fontSize: 24.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                  height: 10,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                    color: Colors.white,
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                    child: Stack(
+                                      children: [
+                                        Container(
+                                          width:
+                                              MediaQuery.of(
+                                                context,
+                                              ).size.width *
+                                              indicatorValue,
+                                          decoration: BoxDecoration(
+                                            gradient: LinearGradient(
+                                              colors: [
+                                                Color(0xFFB96FFF),
+                                                Color(0xFF55CB74),
+                                              ],
+                                            ),
+                                            borderRadius: BorderRadius.circular(
+                                              5.0,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 16),
+                          Image.asset(
+                            'assets/img/Level_badge.png',
+                            width: 48,
+                            height: 48,
+                          ),
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 16.0),
+                        width: double.infinity,
+                        height: 87,
+                        decoration: BoxDecoration(
+                          color: Colors.white12,
+                          borderRadius: BorderRadius.circular(16.0),
+                          border: Border.all(
+                            color: Colors.white.withAlpha(40),
+                            width: 1,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/img/run.png',
+                              width: 87,
+                              height: 87,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16.0),
+                        gradient: LinearGradient(
+                          colors: [Color(0xFF82AFFF), Color(0xFFF14985)],
+                          begin: Alignment.topLeft,
+                          stops: [0.2, 1.5],
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.all(20.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Share & Get',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Get 2x point for every steps, only valid for today',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                    maxLines: 2,
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  ElevatedButton.icon(
+                                    icon: Icon(
+                                      Icons.share,
+                                      color: Colors.white,
+                                    ),
+                                    onPressed: () {},
+                                    label: Text('Share'),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Color(
+                                        0xFF2F3C50,
+                                      ).withAlpha(20),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(
+                                          25.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(16.0),
+                              bottomRight: Radius.circular(16.0),
+                            ),
+                            child: Image.asset(
+                              'assets/img/home_share.png',
+                              fit: BoxFit.cover,
+                              width: 150,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
